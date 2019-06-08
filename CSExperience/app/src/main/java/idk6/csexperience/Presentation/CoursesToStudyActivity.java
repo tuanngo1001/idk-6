187853d605a5c15f1834404da20978bb7b607c34
Kurt Hanel <hanelk@myumanitoba.ca>
2019-06-08 17:36:07 -0500

    Add presentation folder w/ courses
    - Add presentation folder to store java classes related to activities
    - Add inital xml and java files for the select course to study screen. (very bare bones)

---
+++ CSExperience/app/src/main/java/idk6/csexperience/Presentation/CoursesToStudyActivity.java
@@ -0,0 +1,23 @@
+package idk6.csexperience.Presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import idk6.csexperience.R;
+
+public class CoursesToStudyActivity extends AppCompatActivity {
+
+    ListView listOfCourses;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses_to_study);
+
+
+
+    }
+}
