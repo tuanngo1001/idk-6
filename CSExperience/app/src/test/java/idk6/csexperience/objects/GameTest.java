287c4d198f8613d8fba81989aa30d44ed6cafab1
Xinyang Li <surfacebook@TONYsSurfaceBook.localdomain>
2019-06-12 18:22:38 -0500

    Add folders for Bussiness and Objects tests

---
+++ CSExperience/app/src/test/java/idk6/csexperience/objects/GameTest.java
@@ -0,0 +1,19 @@
+package idk6.csexperience.objects;
+
+import org.junit.Test;
+
+import idk6.csexperience.objects.Game;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+    @Test
+    public void testGamePlayer1(){
+        Game gamePlayer;
+        System.out.println("\nStarting testGame");
+        gamePlayer = new Game("playerName");
+        assertNotNull(gamePlayer);
+        assertTrue("playerName".equals(gamePlayer.getPlayer()));
+
+    }
+}
