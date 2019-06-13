8da3ad93afbedc1fea7cba664aa7d9f56c973730
James Froese <froesej9@myumanitoba.ca>
2019-06-13 14:36:50 -0500

    Removed unused Classes from Iteration1

--- CSExperience/app/src/test/java/idk6/csexperience/AllTests.java
+++ CSExperience/app/src/test/java/idk6/csexperience/AllTests.java
@@ -8,9 +8,6 @@ import idk6.csexperience.business.AdjustGameTest;
 import idk6.csexperience.business.AdjustCalendarTest;
 
 import idk6.csexperience.objects.PlayerStatsTest;
-import idk6.csexperience.objects.SkillLevelTest;
-import idk6.csexperience.objects.SkillTest;
-import idk6.csexperience.objects.CombatSkillsTest;
 import idk6.csexperience.objects.GameTest;
 import idk6.csexperience.objects.PlayerTest;
 import idk6.csexperience.objects.CalendarTest;
@@ -24,10 +21,7 @@ import idk6.csexperience.objects.CalendarTest;
         GameTest.class,
         PlayerTest.class,
         PlayerStatsTest.class,
-        CalendarTest.class,
-        SkillTest.class,
-        SkillLevelTest.class,
-        CombatSkillsTest.class
+        CalendarTest.class
 
 })
 
