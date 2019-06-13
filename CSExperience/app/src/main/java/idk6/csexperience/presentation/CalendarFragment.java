032826b92c9478b409f743479c50db34ea2625c3
Renan Turra <renan.turra@gmail.com>
2019-06-13 00:23:11 -0500

    Update CalendarFragment.java

--- CSExperience/app/src/main/java/idk6/csexperience/presentation/CalendarFragment.java
+++ CSExperience/app/src/main/java/idk6/csexperience/presentation/CalendarFragment.java
@@ -1,5 +1,7 @@
 package idk6.csexperience.presentation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import idk6.csexperience.R;
 
@@ -15,7 +18,10 @@ public class CalendarFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_calendar,container,false);
 
-        return inflater.inflate(R.layout.fragment_calendar,container,false);
+
+        return view;
+
     }
 }
