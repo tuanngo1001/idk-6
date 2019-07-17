c6367f039b40e603f73026758a2d6800aaacf218
Renan Turra <renan.turra@gmail.com>
2019-07-17 17:21:28 -0500

    return button fixed.

--- CSExperience/app/src/main/java/idk6/csexperience/presentation/StartActivity.java
+++ CSExperience/app/src/main/java/idk6/csexperience/presentation/StartActivity.java
@@ -17,6 +17,7 @@ public class StartActivity extends AppCompatActivity{
     private Game game;
     private static boolean loaded = false;
     private Button start, load, play;
+    private FloatingActionButton exit;
 
     public void onCreate(Bundle savedInstanceState) {
 
@@ -38,12 +39,21 @@ public class StartActivity extends AppCompatActivity{
         start.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view) {
                 setContentView(R.layout.name_main);
-                play = findViewById(R.id.buttonPlay);
+                play = (Button) findViewById(R.id.buttonPlay);
+                exit = (FloatingActionButton) findViewById(R.id.nameBack);
 
                 play.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         onPlayClick(view);
+                    }
+                });
+
+                exit.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        Intent newIntent = new Intent(getBaseContext(), StartActivity.class);
+                        startActivity(newIntent);
                     }
                 });
 
@@ -104,13 +114,6 @@ public class StartActivity extends AppCompatActivity{
         Intent intent = new Intent(this, LoadActivity.class);
         startActivity(intent);
 
-    }
-
-    protected void onReturnClick(View view) {
-        System.out.println("RETURNCLICK -------------------------------------");
-        System.out.println(game.getPlayer().getName());
-        System.out.println("NAME");
-        setContentView(R.layout.start_main);
     }
 
 }
